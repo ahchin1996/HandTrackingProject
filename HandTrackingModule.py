1e031f5f602717339d9c11ee0913ff44d474aeb9
ahchin1996 <cooker24624872@gmail.com>
2021-08-22 20:58:42 +0800

    add new files

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -15,6 +15,7 @@ class handDetector():
 
     def findHands(self, img, draw = True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # Processes an RGB image and returns the hand landmarks and handedness of each detected hand.
         self.results = self.hands.process(imgRGB)
         # print(results.multi_hand_landmarks)
         if self.results.multi_hand_landmarks:
@@ -25,16 +26,16 @@ class handDetector():
 
     def findPosition(self, img, handNo = 0, draw = True):
         lmList = []
-        if self.results.multi_hand_landmarks:
-            myHand = self.results.multi_hand_landmarks[handNo]
-            for id, lm in enumerate(myHand.landmark):
-                # print(id, lm)
-                h, w, c = img.shape
-                cx, cy = int(lm.x * w), int(lm.y * h) # our position of hte center
-                # print(id, cx, cy)
-                lmList.append([id, cx, cy])
-                if draw:
-                    cv2.circle(img, (cx,cy), 10, (255,0,255), cv2.FILLED)
+        # Collection of detected/tracked hands
+        myHand = self.results.multi_hand_landmarks[handNo]
+        for id, lm in enumerate(myHand.landmark):
+            # print(id, lm)
+            h, w, c = img.shape
+            cx, cy = int(lm.x * w), int(lm.y * h) # our position of hte center
+            # print(id, cx, cy)
+            lmList.append([id, cx, cy])
+            if draw:
+                cv2.circle(img, (cx,cy), 10, (255,0,255), cv2.FILLED)
 
         return lmList
 
